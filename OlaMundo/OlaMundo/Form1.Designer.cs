0ea64acb439c5941136ae17a1be389637d8f5a71
Edson Zeballos <09edson@gmail.com>
2017-07-26 19:02:20 -0400

    Capitulo 1

---
+++ OlaMundo/OlaMundo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace OlaMundo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOk = new System.Windows.Forms.Button();
+			this.btnSair = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnOk
+			// 
+			this.btnOk.Location = new System.Drawing.Point(197, 227);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(75, 23);
+			this.btnOk.TabIndex = 0;
+			this.btnOk.Text = "OK";
+			this.btnOk.UseVisualStyleBackColor = true;
+			this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+			// 
+			// btnSair
+			// 
+			this.btnSair.Location = new System.Drawing.Point(116, 227);
+			this.btnSair.Name = "btnSair";
+			this.btnSair.Size = new System.Drawing.Size(75, 23);
+			this.btnSair.TabIndex = 1;
+			this.btnSair.Text = "SAIR";
+			this.btnSair.UseVisualStyleBackColor = true;
+			this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.btnSair);
+			this.Controls.Add(this.btnOk);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOk;
+		private System.Windows.Forms.Button btnSair;
+	}
+}
+
