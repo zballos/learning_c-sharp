025d0d6b5714eb14047166eb1c7c59fdb157e62e
Edson Zeballos <09edson@gmail.com>
2017-08-03 22:58:25 -0400

    Including labels in form

--- Chapter6/Chapter6/Form2.Designer.cs
+++ Chapter6/Chapter6/Form2.Designer.cs
@@ -34,39 +34,43 @@
             this.textValue = new System.Windows.Forms.TextBox();
             this.btnDep = new System.Windows.Forms.Button();
             this.btnSaq = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtTitular
             // 
             this.txtTitular.Location = new System.Drawing.Point(176, 39);
             this.txtTitular.Name = "txtTitular";
-            this.txtTitular.Size = new System.Drawing.Size(100, 20);
+            this.txtTitular.Size = new System.Drawing.Size(114, 20);
             this.txtTitular.TabIndex = 0;
             // 
             // txtBalance
             // 
             this.txtBalance.Location = new System.Drawing.Point(176, 77);
             this.txtBalance.Name = "txtBalance";
-            this.txtBalance.Size = new System.Drawing.Size(100, 20);
+            this.txtBalance.Size = new System.Drawing.Size(114, 20);
             this.txtBalance.TabIndex = 1;
             // 
             // txtNumber
             // 
             this.txtNumber.Location = new System.Drawing.Point(176, 116);
             this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.Size = new System.Drawing.Size(114, 20);
             this.txtNumber.TabIndex = 2;
             // 
             // textValue
             // 
-            this.textValue.Location = new System.Drawing.Point(176, 172);
+            this.textValue.Location = new System.Drawing.Point(176, 173);
             this.textValue.Name = "textValue";
-            this.textValue.Size = new System.Drawing.Size(100, 20);
+            this.textValue.Size = new System.Drawing.Size(114, 20);
             this.textValue.TabIndex = 3;
             // 
             // btnDep
             // 
-            this.btnDep.Location = new System.Drawing.Point(293, 170);
+            this.btnDep.Location = new System.Drawing.Point(176, 212);
             this.btnDep.Name = "btnDep";
             this.btnDep.Size = new System.Drawing.Size(75, 23);
             this.btnDep.TabIndex = 4;
@@ -76,7 +80,7 @@
             // 
             // btnSaq
             // 
-            this.btnSaq.Location = new System.Drawing.Point(84, 169);
+            this.btnSaq.Location = new System.Drawing.Point(275, 212);
             this.btnSaq.Name = "btnSaq";
             this.btnSaq.Size = new System.Drawing.Size(75, 23);
             this.btnSaq.TabIndex = 5;
@@ -84,11 +88,51 @@
             this.btnSaq.UseVisualStyleBackColor = true;
             this.btnSaq.Click += new System.EventHandler(this.btnSaq_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Titular";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Saldo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Número";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Valor";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(476, 262);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSaq);
             this.Controls.Add(this.btnDep);
             this.Controls.Add(this.textValue);
@@ -111,5 +155,9 @@
         private System.Windows.Forms.TextBox textValue;
         private System.Windows.Forms.Button btnDep;
         private System.Windows.Forms.Button btnSaq;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
