e811872eac6a76159144c89e952557bf4b0bdd61
Edson Zeballos <09edson@gmail.com>
2017-08-14 23:40:58 -0400

    new solution CSharpBrasil; New project ValidadorDeDocumentos

---
+++ CSharpBrasil/ValidadorDeDocumentos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caelum.Stella.CSharp.Validation;
+
+namespace ValidadorDeDocumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpf1 = "31233311109";
+            string cpf2 = "22222222222";
+            string cpf3 = "86288366757";
+
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+            ValidarCPF(cpf3);
+        }
+
+        private static void ValidarCPF(string cpf)
+        {
+            try
+            {
+                new CPFValidator().AssertValid(cpf);
+                Debug.WriteLine("CPF Válido! " + cpf);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("CPF Inválido! " + cpf + " Erro: " + e.ToString());
+            }
+        }
+    }
+}
