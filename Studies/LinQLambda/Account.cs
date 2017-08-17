7d56bdc19614b68c73b6f552f4b79e2ee35c4d28
Edson Zeballos <09edson@gmail.com>
2017-08-16 21:03:38 -0400

    test LinQ and Lambda

---
+++ Studies/LinQLambda/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQLambda
+{
+    class Account
+    {
+        public double Balance { get; private set; }
+
+        public Account(double balance)
+        {
+            this.Balance = balance;
+        }
+    }
+}
---
+++ Studies/LinQLambda/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinQLambda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var myList = new List<Account>();
+
+            myList.Add(AccountWithBalance(3000.00));
+            myList.Add(AccountWithBalance(4500.00));
+            myList.Add(AccountWithBalance(1500.00));
+
+            // example LINQ
+            var filtered =
+                from acc in myList
+                where acc.Balance > 2000.00
+                select acc;
+
+            foreach (Account a in filtered)
+            {
+                MessageBox.Show("O saldo é: " + a.Balance);
+            }
+
+            double total = myList.Sum(acc => acc.Balance);
+            MessageBox.Show("Valor total é: " + total);
+        }
+
+        private Account AccountWithBalance(double v)
+        {
+            return new Account(v);
+        }
+    }
+}
