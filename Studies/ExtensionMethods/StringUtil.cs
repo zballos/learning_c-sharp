d80f860e00267cd4308ce56268a92452f1cea0c8
Edson Zeballos <09edson@gmail.com>
2017-08-17 22:05:01 -0400

    Extension methods : Pluralize

---
+++ Studies/ExtensionMethods/StringUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    static class StringUtil
+    {
+        public static string Pluralize(this string text)
+        {
+            if (text.EndsWith("s"))
+            {
+                return text;
+            }
+            else
+            {
+                return text + "s";
+            }
+        }
+    }
+}
