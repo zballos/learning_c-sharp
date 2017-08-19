a917e08b6af6dd4225fb689e8e1968c83bfd29f8
Edson Zeballos <09edson@gmail.com>
2017-08-18 22:20:11 -0400

    Tests with string functions

---
+++ Studies/StringFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ms = "Avenida Afonso Pena, 1854, Centro, Campo Grande - Mato Grosso do Sul";
+
+            string[] parts = ms.Split(',');
+
+            foreach(string p in parts)
+            {
+                Debug.WriteLine(p);
+            }
+
+            string newAddress = ms.Replace("Centro", "Parque dos Poderes");
+            Debug.WriteLine(newAddress);
+            Debug.WriteLine(newAddress.ToUpper());
+            Debug.WriteLine(newAddress.ToLower());
+            Debug.WriteLine(newAddress.Substring(0, 15));
+            Debug.WriteLine(newAddress.IndexOf("-"));
+
+            string.Format("{0} {1}, what do you {2}?", "Good morning", "Jon T", "Thinking");
+            // or direct
+            Debug.WriteLine("{0} {1}, what do you {2}?", "Good morning", "Jon T", "Thinking");
+        }
+    }
+}
---
+++ Studies/StringFunctions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("StringFunctions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringFunctions")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("da3da753-6e50-4f65-b7ca-96d39d0d835f")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
