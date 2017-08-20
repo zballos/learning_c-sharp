5d591d63d045d9b3d26cac697758ef93e86095f0
Edson Zeballos <09edson@gmail.com>
2017-08-20 19:42:12 -0400

    trabalhando com cep do viacep

---
+++ CSharpBrasil/CEP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cep = "79020200";
+            string url = "https://viacep.com.br/ws/"+ cep +"/json/";
+
+            string result = new HttpClient().GetStringAsync(url).Result;
+            Debug.WriteLine(result);
+        }
+    }
+}
